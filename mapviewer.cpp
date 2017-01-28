247ebcf4033afd6a4a3149792a61c7c6588d5351
Martin Ribelotta <martinribelotta@gmail.com>
2017-01-28 17:25:27 -0300

    MapView code clean
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- mapviewer.cpp
+++ mapviewer.cpp
@@ -1,9 +1,9 @@
+#include "codeeditor.h"
 #include "mapviewer.h"
 
 #include <QFile>
 #include <QGraphicsDropShadowEffect>
 #include <QGraphicsProxyWidget>
-#include <QGraphicsWidget>
 #include <QProgressBar>
 #include <QRegularExpression>
 #include <QRegularExpressionValidator>
@@ -17,8 +17,14 @@ MapViewer::MapViewer(QWidget *parent) : QGraphicsView(parent)
 }
 
 static const QLatin1String MEMORY_MAP_HEADER("Memory Configuration");
-static const QLatin1String regex_mem(R"(^(?P<name>\S+)\s+(?P<origin>0x[0-9a-fA-F]+)\s+(?P<length>0x[0-9a-fA-F]+)\s+(?P<attr>[xrw]+)$)");
-static const QLatin1String regex_sec(R"(^(?!\.debug|\.comment|.*?attributes)(?P<name>\.\S+)\s+(?P<vma>0x[a-f0-9]+)\s+(?!0x0)(?P<size>0x[a-f0-9]+)(?:\s+load address (?P<lma>0x[a-z0-9]+))?)");
+static const QLatin1String REGEX_MEM(R"(^(?P<name>\S+)\s+)"
+                                     R"((?P<origin>0x[0-9a-fA-F]+)\s+)"
+                                     R"((?P<length>0x[0-9a-fA-F]+)\s+)"
+                                     R"((?P<attr>[xrw]+)$)");
+static const QLatin1String REGEX_SEC(R"(^(?!\.debug|\.comment|.*?attributes)(?P<name>\.\S+)\s+)"
+                                     R"((?P<vma>0x[a-f0-9]+)\s+)"
+                                     R"((?!0x0)(?P<size>0x[a-f0-9]+))"
+                                     R"((?:\s+load address (?P<lma>0x[a-z0-9]+))?)");
 
 struct MemoryChunk {
     QString name;
@@ -30,7 +36,7 @@ struct MemoryChunk {
     }
 };
 
-inline QDebug operator<<(QDebug dbg, MemoryChunk chunk) {
+inline QDebug operator<<(QDebug dbg, const MemoryChunk& chunk) {
     dbg << chunk.name
         << ":{ addr:" << chunk.addr
         << "size:" << chunk.size
@@ -44,6 +50,7 @@ struct MemoryRegion {
     uint32_t size;
     QString attr;
     uint32_t used;
+    uint32_t pad;
 
     uint32_t upper() const {
         return base + size;
@@ -54,20 +61,40 @@ struct MemoryRegion {
     }
 };
 
+inline QDebug operator<<(QDebug dbg, const MemoryRegion& m) {
+    dbg << m.name
+        << ":{ base:" << m.base
+        << "size:" << m.size
+        << "attr:" << m.attr
+        << " }";
+    return dbg;
+}
 
 class MemoryBar: public QGraphicsProxyWidget {
 public:
-    MemoryBar(const MemoryRegion& m, QGraphicsItem *parent = 0l): QGraphicsProxyWidget(parent) {
-        auto bar = new QProgressBar();
-        setWidget(bar);
-        bar->setFormat(tr("%1 using %v bytes of %m bytes (%p%)").arg(m.name));
-        bar->setTextVisible(true);
-        bar->setMinimum(0);
-        bar->setMaximum(static_cast<int>(m.size));
-        bar->setValue(static_cast<int>(m.used));
-        bar->resize(QFontMetrics(bar->font()).width(bar->text()) + 100, bar->height());
-    }
+    MemoryBar(const MemoryRegion& m, QGraphicsItem *parent = 0l);
+    virtual ~MemoryBar();
 };
+
+MemoryBar::MemoryBar(const MemoryRegion& m, QGraphicsItem *parent) :
+    QGraphicsProxyWidget(parent)
+{
+    auto bar = new QProgressBar();
+    setWidget(bar);
+    bar->setFormat(tr("%1 using %v bytes of %m bytes (%p%)").arg(m.name));
+    bar->setTextVisible(true);
+    bar->setMinimum(0);
+    bar->setMaximum(static_cast<int>(m.size));
+    bar->setValue(static_cast<int>(m.used));
+    bar->resize(QFontMetrics(bar->font()).width(bar->text()) + 100, bar->height());
+    auto effect = new QGraphicsDropShadowEffect();
+    effect->setBlurRadius(8);
+    effect->setColor(Qt::black);
+    effect->setOffset(0, 0);
+    setGraphicsEffect(effect);
+}
+
+MemoryBar::~MemoryBar() {}
 
 bool MapViewer::load(const QString &path)
 {
@@ -78,7 +105,7 @@ bool MapViewer::load(const QString &path)
         int memHeaderIdx = text.indexOf(MEMORY_MAP_HEADER);
         if (memHeaderIdx > 0) {
             QList<MemoryRegion> memoryRegions;
-            auto re_mem = QRegularExpression(regex_mem, QRegularExpression::CaseInsensitiveOption |
+            auto re_mem = QRegularExpression(REGEX_MEM, QRegularExpression::CaseInsensitiveOption |
                                                         QRegularExpression::MultilineOption);
             auto mem_matchs = re_mem.globalMatch(text, memHeaderIdx);
             while (mem_matchs.hasNext()){
@@ -87,10 +114,10 @@ bool MapViewer::load(const QString &path)
                 auto memOrg = m.captured("origin").remove(0, 2).toUInt(nullptr, 16);
                 auto memLen = m.captured("length").remove(0, 2).toUInt(nullptr, 16);
                 auto memAttr = m.captured("attr");
-                memoryRegions.append(MemoryRegion({memName, memOrg, memLen, memAttr, 0}));
+                memoryRegions.append(MemoryRegion({memName, memOrg, memLen, memAttr, 0, 0 }));
             }
 
-            auto re_sec = QRegularExpression(regex_sec, QRegularExpression::CaseInsensitiveOption |
+            auto re_sec = QRegularExpression(REGEX_SEC, QRegularExpression::CaseInsensitiveOption |
                                                         QRegularExpression::MultilineOption);
             auto sec_matchs = re_sec.globalMatch(text, memHeaderIdx);
             while (sec_matchs.hasNext()) {
@@ -120,7 +147,13 @@ bool MapViewer::load(const QString &path)
                 item->moveBy(0, dy);
                 dy += item->boundingRect().height() + 10;
             }
-
+#if 0
+            auto editor = new CodeEditor();
+            editor->load(path);
+            auto editorWidget = scene()->addWidget(editor);
+            editorWidget->setPos(0, dy + 10);
+            editorWidget->resize(maxWidth, editorWidget->size().height());
+#endif
             return true;
         }
     }
