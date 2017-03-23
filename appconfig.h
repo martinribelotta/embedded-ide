289a6e3f3435cf44b441a224d0a527a9f29b3a06
Alvaro Denis Acosta Quesada <denisacostaq@gmail.com>
2017-03-22 21:55:40 -0400

    ref #5 - Class AppConfig as a singleton to get a only one single access point to all app settings.

---
+++ appconfig.h
@@ -0,0 +1,44 @@
+#ifndef APPCONFIG_H
+#define APPCONFIG_H
+
+#include "configdialog.h"
+
+class AppConfig
+{
+    friend ConfigDialog;
+  public:
+    const QStringList& buildAdditionalPaths() const;
+    const QString& editorStyle() const;
+    int editorFontSize() const;
+    const QString& editorFontStyle() const;
+    bool editorSaveOnAction() const;
+    bool editorTabsToSpaces() const;
+    int editorTabWidth() const;
+    const QString& builDefaultProjectPath() const;
+    const QString& builTemplatePath() const;
+    const QString& builTemplateUrl() const;
+    QString defaultApplicationResources();
+    QString defaultProjectPath();
+    QString defaultTemplatePath();
+
+    void adjustPath();
+
+    static AppConfig& mutable_instance();
+  private:
+    AppConfig();
+    void load();
+    void save();
+
+    void setBuildAdditionalPaths(const QStringList& buildAdditionalPaths) const;
+    void setEditorStyle(const QString& editorStyle);
+    void setEditorFontSize(int editorFontSize);
+    void setEditorFontStyle(const QString& editorFontStyle);
+    void setEditorSaveOnAction(bool editorSaveOnAction);
+    void setEditorTabsToSpaces(bool editorTabsToSpaces);
+    void setEditorTabWidth(int editorTabWidth);
+    void setBuilDefaultProjectPath(const QString& builDefaultProjectPath);
+    void setBuilTemplatePath(const QString& builTemplatePath);
+    void setBuilTemplateUrl(const QString& builTemplateUrl);
+};
+
+#endif // APPCONFIG_H
