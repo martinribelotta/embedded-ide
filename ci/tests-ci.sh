f62f0938c2da1c4be48b78354f88029e562242d7
Martin Ribelotta <martinribelotta@gmail.com>
2019-06-07 18:46:27 -0300

    More ci test
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- ci/tests-ci.sh
+++ ci/tests-ci.sh
@@ -3,12 +3,8 @@
 set -x
 
 source /opt/qt*/bin/qt*-env.sh
-
-mkdir -p /tmp/qsci
-cp ./ci/BuildQSCI.mk /tmp/qsci
-cd /tmp/qsci
-make -f BuildQSCI.mk
-
+pwd
+ls
 /opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info embedded-ide.pro
 make -j
 
