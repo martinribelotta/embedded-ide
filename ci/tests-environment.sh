894a9a096f4590e88c7a6cee3db041803acde684
Martin Ribelotta <martinribelotta@gmail.com>
2019-06-07 20:25:15 -0300

    Add 1GByte of swap for CI
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- ci/tests-environment.sh
+++ ci/tests-environment.sh
@@ -3,15 +3,19 @@
 set -e
 
 sudo add-apt-repository --yes ppa:beineri/opt-qt593-trusty
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 cd /tmp/
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 sudo cp linuxdeployqt-continuous-x86_64.AppImage /usr/bin/linuxdeployqt
 sudo chmod a+x /usr/bin/linuxdeployqt
 cd -
 
-sudo apt-get install -y xpra build-essential qt59base qt59tools qt59svg qt59imageformats qt59x11extras libglu1-mesa-dev wget fuse
+sudo apt-get install -y gcc-7 xpra build-essential qt59base qt59tools qt59svg qt59imageformats qt59x11extras libglu1-mesa-dev wget fuse
 
 mkdir -p /tmp/qsci
 cp ./ci/BuildQSCI.mk /tmp/qsci
