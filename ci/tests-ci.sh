652d99d56d215fada502879a7c2b94f29add837c
Martin Ribelotta <martinribelotta@gmail.com>
2019-06-11 20:32:52 -0300

    Try to compile with gcc-8 (remove qt*)
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- ci/tests-ci.sh
+++ ci/tests-ci.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-# source /opt/qt*/bin/qt*-env.sh
+source /opt/qt*/bin/qt*-env.sh
+qmake --version
+
 INSTALL_DIR=/tmp/embedded-ide
 APP_IMAGE_NAME=Embedded_IDE-x86_64.AppImage
 DEPLOY_OPT=-no-translations -verbose=2 -executable=$INSTALL_DIR/usr/bin/embedded-ide
@@ -10,17 +12,18 @@ DESKTOP_FILE=$INSTALL_DIR/usr/share/applications/embedded-ide.desktop
 
 wget https://github.com/martinribelotta/embedded-ide-builder/blob/master/linux-x86_64/universal-ctags?raw=true -O /tmp/universal-ctags
 
+
 pwd && ls
 /opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info embedded-ide.pro
 make -j
 make install INSTALL_ROOT=${INSTALL_DIR}
-linuxdeployqt $DESKTOP_FILE -qmake=/opt/qt*/bin/qmake $DEPLOY_OPT -appimage
-linuxdeployqt $INSTALL_DIR/usr/bin/qtshdialog -no-translations -verbose=2 -no-plugins -qmake=/opt/qt*/bin/qmake
-linuxdeployqt $INSTALL_DIR/usr/bin/socketwaiter -no-translations -verbose=2 -no-plugins -qmake=/opt/qt*/bin/qmake
+linuxdeployqt $DESKTOP_FILE $DEPLOY_OPT -appimage
+linuxdeployqt $INSTALL_DIR/usr/bin/qtshdialog -no-translations -verbose=2 -no-plugins
+linuxdeployqt $INSTALL_DIR/usr/bin/socketwaiter -no-translations -verbose=2 -no-plugins
 cp /opt/qt*/lib/libQt5Svg.so.5 $INSTALL_DIR/usr/lib
 cp /opt/qt*/plugins/imageformats/libqsvg.so $INSTALL_DIR/usr/plugins/imageformats/
 cp /tmp/universal-ctags $INSTALL_DIR/usr/bin
 chmod a+x $INSTALL_DIR/usr/bin/universal-ctags
-linuxdeployqt $DESKTOP_FILE -qmake=/opt/qt*/bin/qmake -appimage
+linuxdeployqt $DESKTOP_FILE -appimage
 
 set -e
