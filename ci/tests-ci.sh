dcf0b1c60ea7014015b80ffdaf08c38959b962ae
Martin Ribelotta <martinribelotta@gmail.com>
2019-06-12 22:01:10 -0300

    Clean CI and add configs in win/linux
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- ci/tests-ci.sh
+++ ci/tests-ci.sh
@@ -20,8 +20,65 @@ wget https://github.com/martinribelotta/embedded-ide-builder/blob/master/linux-x
 echo ************** LINUX BUILD ***********************
 
 /opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info embedded-ide.pro
-make -j
+make -j4
 make install INSTALL_ROOT=${INSTALL_DIR}
+
+cat > ${INSTALL_DIR}/usr/share/embedded-ide/embedded_ide-config.json <<"EOF"
+{
+   "workspacePath": "${APPLICATION_DIR_PATH}/../../../embedded-ide-workspace"
+}
+EOF
+
+cat > ${INSTALL_DIR}/usr/share/embedded-ide/embedded-ide.hardconf <<"EOF"
+{
+        "additionalPaths": [
+           "${APPLICATION_DIR_PATH}",
+           "${APPLICATION_DIR_PATH}/../../../tools/gcc-arm-embedded/bin",
+           "${APPLICATION_DIR_PATH}/../../../tools/openocd/bin",
+           "${APPLICATION_DIR_PATH}/../../../tools/system/bin"
+        ],
+        "editor": {
+            "font": {
+                "name": "Ubuntu Mono",
+                "size": 12
+            },
+            "formatterStyle": "linux",
+            "saveOnAction": false,
+            "style": "Default",
+            "tabWidth": 3,
+            "tabsOnSpaces": true
+        },
+        "externalTools": [
+            { "Install FTDI drivers": "bash ${APPLICATION_DIR_PATH}/ftdi-tools.sh --install" },
+            { "Uninstall FTDI drivers": "bash ${APPLICATION_DIR_PATH}/ftdi-tools.sh --uninstall" },
+            { "Add desktop integration": "bash ${APPLICATION_DIR_PATH}/desktop-integration.sh --install" },
+            { "Remove desktop integration": "bash ${APPLICATION_DIR_PATH}/desktop-integration.sh --uninstall" }
+        ],
+        "history": [ ],
+        "logger": {
+            "font": {
+                "name": "Ubuntu Mono",
+                "size": 10
+            }
+        },
+        "network": {
+            "proxy": {
+                "host": "",
+                "pass": "",
+                "port": "",
+                "type": "None",
+                "useCredentials": false,
+                "user": ""
+            }
+        },
+        "templates": {
+            "autoUpdate": true,
+            "url": "https://api.github.com/repos/ciaa/EmbeddedIDE-templates/contents"
+        },
+        "useDevelopMode": false
+}
+EOF
+
 linuxdeployqt $DESKTOP_FILE $DEPLOY_OPT -appimage
 linuxdeployqt $INSTALL_DIR/usr/bin/qtshdialog -no-translations -verbose=2 -no-plugins
 linuxdeployqt $INSTALL_DIR/usr/bin/socketwaiter -no-translations -verbose=2 -no-plugins
@@ -42,7 +99,64 @@ ${MXEQT}/bin/qmake CONFIG+=release CONFIG+=force_debug_info embedded-ide.pro
 make -j4
 pydeployqt --objdump ${MXE_PREFIX}-objdump ${PWD}/build/embedded-ide.exe \
 	--libs ${MXE}/${MXE_PREFIX}/bin/:${MXEQT}/bin/:${MXEQT}/lib/ \
-	--extradll Qt5Svg.dll:libjpeg-9.dll \
+	--extradll Qt5Svg.dll:Qt5Qml.dll:libjpeg-9.dll \
 	--qmake ${MXEQT}/bin/qmake
 mv build embedded-ide
+
+cat > embedded-ide/embedded_ide-config.json <<"EOF"
+{
+   "workspacePath": "${APPLICATION_DIR_PATH}/../embedded-ide-workspace"
+}
+EOF
+
+cat > embedded-ide/embedded-ide.hardconf <<"EOF"
+{
+        "additionalPaths": [
+           "${APPLICATION_DIR_PATH}",
+           "${APPLICATION_DIR_PATH}/../tools/arm-none-eabi-gcc/bin",
+           "${APPLICATION_DIR_PATH}/../tools/openocd/bin",
+           "${APPLICATION_DIR_PATH}/../tools/system",
+           "${APPLICATION_DIR_PATH}/../tools/zenity",
+           "${APPLICATION_DIR_PATH}/../tools/drivers"
+           "${APPLICATION_DIR_PATH}/../tools/serial-terminal"
+        ],
+        "editor": {
+            "font": {
+                "name": "Ubuntu Mono",
+                "size": 12
+            },
+            "formatterStyle": "linux",
+            "saveOnAction": false,
+            "style": "Default",
+            "tabWidth": 3,
+            "tabsOnSpaces": true
+        },
+        "externalTools": [
+            { "Launch Zadig": "cmd /C start zadigv2.0.1.154.exe" },
+            { "Serial Terminal": "cmd /C start Terminal.exe" }
+        ],
+        "history": [ ],
+        "logger": {
+            "font": {
+                "name": "Ubuntu Mono",
+                "size": 10
+            }
+        },
+        "network": {
+            "proxy": {
+                "host": "",
+                "pass": "",
+                "port": "",
+                "type": "None",
+                "useCredentials": false,
+                "user": ""
+            }
+        },
+        "templates": {
+            "autoUpdate": true,
+            "url": "https://api.github.com/repos/ciaa/EmbeddedIDE-templates/contents"
+        },
+        "useDevelopMode": false
+}
+EOF
 zip -9 -r Embedded_IDE-${VERSION}-win32.zip embedded-ide
